229b69ab7b29edba47bd5fc12684f852b9581cc5
Esh <32418603+Esh07@users.noreply.github.com>
2024-07-27 23:12:22 +0530

    add: Add Jwt implementation with register route

--- backend/src/app/index.ts
+++ backend/src/app/index.ts
@@ -5,11 +5,29 @@
 const compression = require('compression')
 import express from 'express'
 const bodyParser = require('body-parser')
+const router = express.Router();
 
-const app = express()
-app.use(bodyParser.json())
-app.use(compression())
+
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(compression());
 
 // A route declaration section
+const { auth } = require('../api/auth/auth.routes');
+console.log(auth)
+app.use('/auth', auth);
+
+const { user } = require('../api/users/users.routes');
+// console.log(user)
+// app.use('/user', user);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+    console.log('Hello World!')
+}
+);
+
 
 export { app };
