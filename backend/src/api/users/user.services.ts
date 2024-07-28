7861a3e31ff2f5cb49370d2d6616b09e6481b4b2
Esh <32418603+Esh07@users.noreply.github.com>
2024-07-28 11:50:29 +0530

    fix: Add TS types to function returns and update ES6 imports

--- backend/src/api/users/user.services.ts
+++ backend/src/api/users/user.services.ts
@@ -1,33 +1,51 @@
 const bcrypt = require('bcrypt');
-const { db } = require('../../lib/db');
+import { User } from '@prisma/client';
+import db from '../../lib/db';
 
-function findUserByEmail(email: string) {
-  return db.user.findUnique({
-    where: {
-      email,
-    },
-  });
+function findUserByEmail(email: string) : Promise<User | null> {
+  // console.log(db); // Add this line to check if db is correctly initialized
+  try {
+    const user = db.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new Error('User not found code line 17');
+    }
+    return user;
+  }
+  catch (err) {
+    console.log(err);
+    throw new Error('User not found');
+    // return db.user.findUnique({
+    //   where: {
+    //     email,
+    //   },
+    // });
+  }
 }
 
-function createUserByEmailAndPassword(user: { email: string; password: string }) {
-  user.password = bcrypt.hashSync(user.password, 12);
-  return db.user.create({
-    data: user,
-  });
-}
+  function createUserByEmailAndPassword(user: { email: string; password: string }) {
+    user.password = bcrypt.hashSync(user.password, 12);
+    return db.user.create({
+      data: user,
+    });
+  }
 
-function findUserById(id: string) {
-  return db.user.findUnique({
-    where: {
-      id,
-    },
-  });
-}
+  function findUserById(id: string): Promise<User | null> {
+    return db.user.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
 
-// module.exports = {
-//   findUserByEmail,
-//   findUserById,
-//   createUserByEmailAndPassword
-// };
+  // module.exports = {
+  //   findUserByEmail,
+  //   findUserById,
+  //   createUserByEmailAndPassword
+  // };
 
-export default { findUserByEmail, findUserById, createUserByEmailAndPassword };
+  export { findUserByEmail, findUserById, createUserByEmailAndPassword };
