8087237503deb82c9a0763c8659f66de9ec1ca61
Esh <32418603+Esh07@users.noreply.github.com>
2024-08-06 10:32:12 +0530

    fix: Fix svg syntax error in Dashboard

--- frontend-app/src/features/auth/Dashboard.tsx
+++ frontend-app/src/features/auth/Dashboard.tsx
@@ -92,7 +92,7 @@ const Dashboard: React.FC = () => {
                     <span className="sr-only">Action button</span>
                     Action
                     <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
                     </svg>
                 </button>
 
@@ -101,7 +101,7 @@ const Dashboard: React.FC = () => {
                 <div className="relative">
                     <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
                         <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                         </svg>
                     </div>
                     <input type="text" 
@@ -122,8 +122,7 @@ const Dashboard: React.FC = () => {
                             </div>
                         </th>
                         <th scope="col" className="px-6 py-3">Id</th>
-                        <th scope="col" className="px-6 py-3">Name</th>
-                        <th scope="col" className="px-6 py-3">Is Admin</th>
+                        <th scope="col" className="px-6 py-3 w-28">Is Admin</th>
                         <th scope="col" className="px-6 py-3">createdAt</th>
                         <th scope="col" className="px-6 py-3">updatedAt</th>
                     </tr>
